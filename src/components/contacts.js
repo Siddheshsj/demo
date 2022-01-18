053191c43e07860300c4fa8074ac58422b1507e8
Siddhesh <siddhesh.shirodkar@sjinnovation.com>
2022-01-18 20:07:37 +0530

    useeffect with photos api

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import Image from './image';
 
 const Contacts = ({ contacts }) => {
+
     return (
       <div>
         <center><h1>Contact List</h1></center>
-        
+        <div className='row'>
         {contacts.map((contact) => (
           // { <div><pre>{JSON.stringify(contacts, null, 2) }</pre></div>}
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">{contact.name}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
-              <p class="card-text">{contact.company.catchPhrase}</p>
+          <div className='col-md-4'>
+            <div className="card" key={contact.id}>
+              <Image id={contact.id} />
+              <div className="card-body">
+                <h5 className="card-title">{contact.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{contact.email}</h6>
+                <p className="card-text">{contact.company.catchPhrase}</p>
+              </div>
             </div>
           </div>
         ))}
+        </div>
       </div>
     )
   };
